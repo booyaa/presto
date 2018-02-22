e6652a57f13be360773161c9f8eef6444f6588fc
booyaa <booyaabooyaabooyaa@gmail.com>
2018-02-22 17:57:50 +0000

    (refactor) remove duff paths

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ fn demo(cookie: &Cookie) {
         "Cargo.toml",
         ".vscode/settings.json",
         "target/debug/presto",
-        "../../rust/learn-intermezzos/build/boot.o",
-        "../../rust/learn-intermezzos/build/os.iso",
+        ".",
     ];
 
     for file in files.iter() {
@@ -18,33 +17,33 @@ fn demo(cookie: &Cookie) {
 }
 
 #[allow(dead_code)]
-fn demo_vec(cookie: &Cookie) {
+fn mc_hammer(cookie: &Cookie) {
     let mut files_vec = vec![
         "Cargo.toml",
         ".vscode/settings.json",
         "target/debug/presto",
-        "../../rust/learn-intermezzos/build/boot.o",
-        "../../rust/learn-intermezzos/build/os.iso",
+        ".",
     ];
 
     for file in &files_vec {
-        println!("{}: {}", file, cookie.file(&file).unwrap());
+        println!("{}:\n\t{}", file, &cookie.file(&file).unwrap());
     }
 
-    files_vec.push(".gitignore");
+    files_vec.push(".gitignore"); // <-- vec ftw!
 
     for file in &files_vec {
-        println!("{}: {}", file, cookie.file(&file).unwrap());
+        println!("{}:\n\t{}", file, cookie.file(&file).unwrap());
     }
 }
 fn main() {
     let cookie = Cookie::open(CookieFlags::default()).unwrap();
-    cookie.load(&[""; 0]).unwrap(); // wtf? found in https://github.com/jkcclemens/bins/blob/42caed1d6f7a34dffd38e75b1f0ec32c7d44c87e/src/main.rs#L524
-
+    cookie.load(&[""; 0]).unwrap();
+    
     let args: Vec<String> = env::args().collect(); // why can't i do this? let args = env::args();
 
-    if args.len() < 2 {
-        // no file passed, run demo mode.
+    let demo_mode = args.len() < 2;
+
+    if demo_mode {
         demo(&cookie);
     } else {
         let file = &args[1];
